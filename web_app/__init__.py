3a50abedf6e78b72eb921df6e4a5be1c2e79ae20
mjortiz01 <mjo81@georgetown.edu>
2024-12-04 20:59:04 -0500

    Web (#2)
    * Web app creation
    
    * Update Readme and Restarted Web App due to issues
    
    * Added route folder and templates
    
    * Implemented bootstrap
    
    * Stocks dashboard added
    
    * Boostrap implemented
    
    * Unemployment Dashboard
    
    * Added deploying instructions

---
+++ web_app/__init__.py
@@ -0,0 +1,20 @@
+# this is the "web_app/__init__.py" file...
+
+from flask import Flask
+import os
+
+from web_app.routes.home_routes import home_routes
+from web_app.routes.stock_routes import stocks_routes
+from web_app.routes.unemployment_routes import unemployment_routes
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(home_routes)
+    app.register_blueprint(stocks_routes)
+    app.register_blueprint(unemployment_routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
