c3f3711d5c9ecf9c0e16b5058d786738de46ff59
mjortiz01 <mjo81@georgetown.edu>
2024-12-04 20:57:37 -0500

    Unemployment Dashboard

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,14 +1,18 @@
 # this is the "web_app/__init__.py" file...
 
 from flask import Flask
+import os
 
 from web_app.routes.home_routes import home_routes
-from web_app.routes.stocks_routes import stocks_routes
+from web_app.routes.stock_routes import stocks_routes
+from web_app.routes.unemployment_routes import unemployment_routes
 
 def create_app():
     app = Flask(__name__)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(stocks_routes)
+    app.register_blueprint(unemployment_routes)
     return app
 
 if __name__ == "__main__":
